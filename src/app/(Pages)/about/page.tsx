a02a8e560a9226981be135a528c3917aa9405661
chanyoungcoding <white1614@naver.com>
2024-07-30 17:27:10 +0900

    start

--- src/app/(Pages)/about/page.tsx
+++ src/app/(Pages)/about/page.tsx
@@ -1,8 +1,7 @@
-
 export default function Home() {
   return (
     <div className="p-2 bg-slate-500 h-5">
-      <p className="text-blue-400 text-xl font-bold">hello</p>
+      <p className="text-blue-400 text-xl font-bold">about</p>
     </div>
   );
 }
---
+++ src/app/(Pages)/mypage/page.tsx
@@ -0,0 +1,9 @@
+const Mypage = () => {
+  return (
+    <div className="p-2 bg-slate-500 h-5">
+      <p className="text-blue-400 text-xl font-bold">mypage</p>
+    </div>
+  );
+}
+
+export default Mypage;
