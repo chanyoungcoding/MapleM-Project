66a2c1b3fce60384563921c20542a8f4839fcf49
chanyoungcoding <white1614@naver.com>
2024-08-06 23:04:08 +0900

    함수 테스트

--- src/app/_components/maple-ability.tsx
+++ src/app/_components/maple-ability.tsx
@@ -10,13 +10,18 @@ interface MapleAbilityProps {
 
 const MapleAbility:React.FC<MapleAbilityProps> = ({stats}) => {
 
+  const statName = (num: number) => {
+    const userStat = stats && stats[num].stat_name
+    return userStat
+  }
+
   return (
     <div className="relative w-[300px] h-[320px] bg-white p-5 border-4 border-maple-sky rounded-lg z-50">
       <h1 className=" pb-5 font-bold text-xl text-maple-sky">스탯</h1>
       
       <div className="flex items-center justify-between">
         <div>
-          <p>{stats && stats[1].stat_name}</p>
+          <p>{statName(1)}</p>
           <p>{stats && stats[1].stat_value}</p>
           
         </div>
